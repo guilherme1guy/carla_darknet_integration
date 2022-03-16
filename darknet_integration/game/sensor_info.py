02327be6aec7b99743fb978474a6f5db192ba0b3
Guilherme Guy <guilhermeguy349@gmail.com>
2022-03-16 18:03:01 -0300

    Add camera distance to sensor info

--- darknet_integration/game/sensor_info.py
+++ darknet_integration/game/sensor_info.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import carla
 
 
@@ -6,30 +7,42 @@ class SensorInfo:
     Data object with sensor info needed by other objects
     """
 
-    def __init__(self, z, pitch, image_size_x, image_size_y, fov) -> None:
+    def __init__(
+        self,
+        z,
+        pitch,
+        image_size_x,
+        image_size_y,
+        fov,
+        camera_distance: Tuple[float, float, float] = (0.0, 0.0, 0.0),
+    ) -> None:
         self.z = z
         self.pitch = pitch
         self.image_size_x = image_size_x
         self.image_size_y = image_size_y
         self.fov = fov
+        self.camera_distance = camera_distance
 
     def __str__(self) -> str:
         return f"SensorInfo(\
-            z={self.z}, \
-            pitch={self.pitch}, \
-            image_size_x={self.image_size_x}, \
-            image_size_y={self.image_size_y}, \
-            fov={self.fov})"
+            \nz={self.z}, \
+            \npitch={self.pitch}, \
+            \nimage_size_x={self.image_size_x}, \
+            \nimage_size_y={self.image_size_y}, \
+            \nfov={self.fov}) \
+            \ncamera_distance={self.camera_distance})\n"
 
     @staticmethod
-    def get_sensor_info(sensor: carla.Sensor) -> "SensorInfo":
-
-        transform = sensor.get_transform()
+    def get_sensor_info(
+        sensor: carla.Sensor,
+        camera_distance: Tuple[float, float, float] = (0.0, 0.0, 0.0),
+    ) -> "SensorInfo":
 
         return SensorInfo(
-            round(transform.location.z * 100, 2),
-            abs(round(transform.rotation.pitch, 2)),
+            round(sensor.parent.bounding_box.extent.y * 2 * 100, 2),
+            abs(round(sensor.get_transform().rotation.pitch, 2)),
             int(sensor.attributes["image_size_x"]),
             int(sensor.attributes["image_size_y"]),
             float(sensor.attributes["fov"]),
+            camera_distance,
         )
