3b49489aaddce56b751bb29e112c25c9ea902f52
Guilherme Guy <guilhermeguy349@gmail.com>
2022-04-12 13:37:32 -0300

    Add YoloV4

--- darknet_integration/yolo/yolo_config.py
+++ darknet_integration/yolo/yolo_config.py
@@ -5,8 +5,12 @@ from pytorchyolo import detect, models
 from pytorchyolo.utils.utils import load_classes
 import torch
 import numpy as np
+import cv2
 
 from yolo.detection import Detection
+
+from yolov4.tool.darknet2pytorch import Darknet as DarknetV4
+from yolov4.tool.torch_utils import do_detect as do_detect_v4
 
 
 class YoloConfig(ABC):
@@ -86,6 +90,77 @@ class YoloV3Config(YoloConfig):
         ]
 
 
+class YoloV4Config(YoloConfig):
+    def __init__(
+        self,
+        conf_threshold=0.7,
+        nms_threshold=0.6,
+        cfg_file="models/yolov4.cfg",
+        weights_file="models/yolov4.weights",
+        classes_file="models/coco.names",
+    ):
+        super().__init__(
+            conf_threshold,
+            nms_threshold,
+        )
+
+        self._cfgfile = cfg_file
+        self._weightsfile = weights_file
+        self._classes = load_classes(classes_file)
+        self.use_cuda = torch.cuda.is_available()
+
+        self.img_shape = (0, 0)
+
+        # set seed to get the same colors for each run
+        np.random.seed(0)
+        self._colors = np.random.uniform(0, 120, size=(len(self._classes), 3))
+
+    def get_model(self):
+        model = DarknetV4(self.cfg_file)
+        model.load_weights(self.weights_file)
+
+        if self.use_cuda:
+            model.cuda()
+
+        return model
+
+    def get_detection_fn(self, model) -> Callable:
+        def fn(img):
+
+            self.img_shape = img.shape[:2]
+
+            sized = cv2.resize(img, (model.width, model.height))
+            sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+
+            outputs = do_detect_v4(
+                model,
+                sized,
+                self.conf_threshold,
+                self.nms_threshold,
+                use_cuda=self.use_cuda,
+            )
+
+            return outputs
+
+        return fn
+
+    def get_output_conversion_fn(self) -> Callable:
+        def fn(element):
+            conv_element = [
+                element[0] * self.img_shape[1],
+                element[1] * self.img_shape[0],
+                element[2] * self.img_shape[1],
+                element[3] * self.img_shape[0],
+                element[4],
+                element[5],
+            ]
+            return Detection.from_output(conv_element)
+
+        return lambda image_outputs: [
+            [fn(element) for element in output[0]] for output in image_outputs
+        ]
+
+
 class YoloV5Config(YoloConfig):
     def __init__(
         self, conf_threshold=0.7, nms_threshold=0.6, classes_file="models/coco.names"
@@ -102,7 +177,9 @@ class YoloV5Config(YoloConfig):
         self._colors = np.random.uniform(0, 120, size=(len(self._classes), 3))
 
     def get_model(self):
-        model = torch.hub.load("ultralytics/yolov5", "yolov5s6", pretrained=True)
+        model = torch.hub.load(
+            "ultralytics/yolov5", "yolov5s6", pretrained=True, verbose=False
+        )
         model.conf = self.conf_threshold
         model.ioy = self.nms_threshold
 
